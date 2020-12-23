3f56bc49f95c861fd903d5fab8fdcbbedf21d312
hasimy-as <ahmadhasyimi674@gmail.com>
2020-12-23 21:07:52 +0700

    Updated api response

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const logger = require('./app/lib/logger');
-const { STATUS, CODE } = require('./app/lib/index');
+const { CODE } = require('./app/lib/index');
 const env = require('./app/config/config');
 const users = require('./app/api/user');
 const app = express();
@@ -11,17 +11,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', users);
 app.get('/', (req, res) =>
 	res.status(CODE.SUCCESS).json({
-		status: STATUS.SUCCESS,
 		message: 'This server is running properly',
 		data: null,
-		code: CODE.SUCCESS,
 	}),
 ).get('*', (req, res) => {
 	res.status(CODE.NOT_FOUND).json({
-		status: STATUS.NOT_FOUND,
 		message: 'cannot get unspecified endpoint',
 		data: null,
-		code: CODE.NOT_FOUND,
 	});
 });
 
